20b89df3b4064da2711346b386f1ed37c9b77780
sokhnabarry <sokhnabarry3@gmail.com>
2026-03-08 15:23:29 +0000

    Ajout MessageService dans le package service - envoi et livraison messages

---
+++ src/main/java/g2/messagerieserver/service/MessageService.java
@@ -0,0 +1,38 @@
+package g2.messagerieserver.service;
+
+import g2.messagerieserver.model.Message;
+import g2.messagerieserver.model.User;
+import g2.messagerieserver.repository.MessageRepository;
+
+import java.util.List;
+
+public class MessageService {
+
+    private final MessageRepository messageRepository = new MessageRepository();
+
+    public Message send(User sender, User receiver, String contenu) throws Exception {
+        if (contenu == null || contenu.isBlank())
+            throw new Exception("Le message ne peut pas être vide. (RG7)");
+        if (contenu.length() > 1000)
+            throw new Exception("Le message ne doit pas dépasser 1000 caractères. (RG7)");
+
+        Message message = new Message(sender, receiver, contenu);
+        return messageRepository.save(message);
+    }
+
+    public List<Message> getConversation(String username1, String username2) {
+        return messageRepository.findConversation(username1, username2);
+    }
+
+    public List<Message> getPendingMessages(String username) {
+        return messageRepository.findPendingMessages(username);
+    }
+
+    public void markAsReceived(Long messageId) {
+        messageRepository.updateStatut(messageId, Message.Statut.RECU);
+    }
+
+    public void markAsRead(Long messageId) {
+        messageRepository.updateStatut(messageId, Message.Statut.LU);
+    }
+}
