504b4d38c8152753cb0e7776e88d0915a9227ed7
sokhnabarry <sokhnabarry3@gmail.com>
2026-03-08 15:45:32 +0000

    Serveur fonctionnel - port 9090 en attente de connexions

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module g2.messagerieserver {
-    requires lombok;
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
     requires java.naming;
     requires java.sql;
-    requires static jbcrypt;
-
+    requires jbcrypt;
+    requires static lombok;
 }
