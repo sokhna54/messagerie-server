39fdb28a917fa076f3c5bbcfd66616efe3ea3b06
zey <zeynadieng10@gmail.com>
2026-03-09 17:28:49 +0000

    Ajout mainAm

---
+++ src/main/java/g2/messagerieclient/MainApp.java
@@ -0,0 +1,23 @@
+package g2.messagerieclient;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/g2/messagerieclient/login.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("G2 Messagerie");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
