3bf8d2987822eda261df10cd0d46402522f834f1
sokhnabarry <sokhnabarry3@gmail.com>
2026-03-09 18:58:55 +0000

    Mise a jour module-info - export packages client JavaFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,14 @@
 module g2.messagerieserver {
+    requires javafx.controls;
+    requires javafx.fxml;
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
     requires java.naming;
     requires java.sql;
     requires jbcrypt;
     requires static lombok;
-    requires javafx.graphics;
-    requires javafx.fxml;
+
+    exports g2.messagerieclient to javafx.graphics;
+    opens g2.messagerieclient to javafx.fxml;
+    opens g2.messagerieclient.controller to javafx.fxml;
 }
