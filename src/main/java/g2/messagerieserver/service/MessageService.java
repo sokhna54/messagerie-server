157bfe1755a5f2547e2a11d681afcd5d2abf3e50
sokhnabarry <sokhnabarry3@gmail.com>
2026-03-13 01:31:45 +0000

    Ajout statuts En ligne/Hors ligne, double trait messages, correction double connexion

--- src/main/java/g2/messagerieserver/service/MessageService.java
+++ src/main/java/g2/messagerieserver/service/MessageService.java
@@ -12,13 +12,14 @@ public class MessageService {
 
     public Message send(User sender, User receiver, String contenu) throws Exception {
         if (contenu == null || contenu.isBlank())
-            throw new Exception("Le message ne peut pas être vide. (RG7)");
+            throw new Exception("Le message ne peut pas être vide. ");
         if (contenu.length() > 1000)
-            throw new Exception("Le message ne doit pas dépasser 1000 caractères. (RG7)");
+            throw new Exception("Le message ne doit pas dépasser 1000 caractères. ");
 
         Message message = new Message(sender, receiver, contenu);
         return messageRepository.save(message);
     }
+    //  Récupérer l'historique entre deux utilisateurs, trié par date
 
     public List<Message> getConversation(String username1, String username2) {
         return messageRepository.findConversation(username1, username2);
